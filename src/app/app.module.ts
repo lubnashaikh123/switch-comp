5695ddd8392f49dfcdaf0c97f93432bf0236e40c
lubnashaikh123 <128475990+lubnashaikh123@users.noreply.github.com>
2023-08-08 20:03:20 +0530

    switch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgSwitchComponent } from './shared/components/ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule
